17fc12804c81367685acfae70101f9509ad45aa0
zoom26042604 <nathan.ferre@ynov.com>
2025-03-04 16:32:54 +0100

    feat(java): Hello World / Loops / Arrays done for the Java Training Exercices

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,9 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/fr/ynov/java/easy/ScanMe.java
@@ -0,0 +1,20 @@
+package fr.ynov.java.easy;
+
+import java.util.Scanner;
+
+public class ScanMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        scanner.close();
+    }
+}
