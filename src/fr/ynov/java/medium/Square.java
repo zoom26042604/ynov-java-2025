287e5cbf66515d1ee34398e1b7bde421038cf56b
zoom26042604 <nathan.ferre@ynov.com>
2025-03-04 16:59:04 +0100

    feat(java): add Square class with square method

---
+++ src/fr/ynov/java/medium/Square.java
@@ -0,0 +1,11 @@
+package fr.ynov.java.medium;
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println(new Square().square(57));
+    }
+
+    public static double square(int num) {
+        return num * num;
+    }
+}
