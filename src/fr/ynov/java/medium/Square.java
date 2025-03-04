134df300aee38eb9360e5ac4a6864ab85566a8dd
zoom26042604 <nathan.ferre@ynov.com>
2025-03-04 17:05:32 +0100

    feat(java): add Square class with square method and the scan of the user input

--- src/fr/ynov/java/medium/Square.java
+++ src/fr/ynov/java/medium/Square.java
@@ -1,8 +1,17 @@
 package fr.ynov.java.medium;
 
+import java.util.Scanner;
+
 public class Square {
     public static void main(String[] args) {
-        System.out.println(new Square().square(57));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Square of " + num + " is: " + square(num));
+
+        scanner.close();
     }
 
     public static double square(int num) {
